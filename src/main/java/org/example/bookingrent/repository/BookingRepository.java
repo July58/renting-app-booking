4be69e54af0cd979ace34584479797d54a2acd0e
yuliia.perevozniuk.hw <yuliia.perevozniuk@prjheadway.com>
2025-08-24 08:12:21 +0300

    created repo booking

---
+++ src/main/java/org/example/bookingrent/repository/BookingRepository.java
@@ -0,0 +1,19 @@
+package org.example.bookingrent.repository;
+
+
+import org.example.bookingrent.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    @Query("SELECT b FROM Booking b WHERE b.id = :itemId " +
+            "AND (b.fromDate <= :endDate AND b.toDate >= :startDate)")
+    List<Booking> findOverlappingBookings(String itemId, LocalDate startDate, LocalDate endDate);
+}
+

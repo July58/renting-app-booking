1674acf27747937b9164f82bd31822ab2ed4ba67
yuliia.perevozniuk <perevyul@cvut.cz>
2025-08-27 19:56:20 +0200

    load env

--- src/main/java/org/example/bookingrent/BookingRentApplication.java
+++ src/main/java/org/example/bookingrent/BookingRentApplication.java
@@ -1,13 +1,35 @@
 package org.example.bookingrent;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BookingRentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BookingRentApplication.class, args);
+        SpringApplication app = new SpringApplication(BookingRentApplication.class);
+        app.addInitializers(new DotenvInitializer());
+        app.run(args);
     }
 
+    static class DotenvInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext ctx) {
+            Dotenv dotenv = Dotenv.configure()
+                    .directory("./")
+                    .ignoreIfMissing()
+                    .load();
+
+            dotenv.entries().forEach(e -> {
+                String key = e.getKey();
+                if (System.getProperty(key) == null && System.getenv(key) == null) {
+                    System.setProperty(key, e.getValue());
+                }
+            });
+        }
+    }
 }
+

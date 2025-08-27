34e9cb9f11bc1f96b9fb9b436ce7a480aec9b7af
yuliia.perevozniuk <perevyul@cvut.cz>
2025-08-27 19:57:09 +0200

    logging filter

---
+++ src/main/java/org/example/bookingrent/config/LoggingFilter.java
@@ -0,0 +1,28 @@
+package org.example.bookingrent.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+@Component
+public class LoggingFilter extends GenericFilterBean {
+
+    private static final Logger logger = LoggerFactory.getLogger("GLOBAL_LOGGER");
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws java.io.IOException, jakarta.servlet.ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        logger.info("Incoming request: {} {}", req.getMethod(), req.getRequestURI());
+
+        chain.doFilter(request, response);
+
+        logger.info("Completed request: {} {}", req.getMethod(), req.getRequestURI());
+    }
+}
+

7e650dd64e3e8f88cf253bc1fab753b9af4ba251
yuliia.perevozniuk <perevyul@cvut.cz>
2025-09-04 23:47:28 +0200

    fixed

--- src/main/java/org/example/bookingrent/config/logging/LoggingFilter.java
+++ src/main/java/org/example/bookingrent/config/logging/LoggingFilter.java
@@ -13,7 +13,7 @@ import org.springframework.web.filter.GenericFilterBean;
 @Component
 public class LoggingFilter extends GenericFilterBean {
 
-    private static final Logger logger = LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
+    private static final Logger logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws java.io.IOException, jakarta.servlet.ServletException {
